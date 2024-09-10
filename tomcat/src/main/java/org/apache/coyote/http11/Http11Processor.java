63b29167e49cdfde07ecafb8ffeb3d91a5bc665b
Taaate <an981022@naver.com>
2024-09-10 16:34:30 +0900

    feat: GET index.html 응답하기

--- tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
+++ tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
@@ -1,6 +1,11 @@
 package org.apache.coyote.http11;
 
 import com.techcourse.exception.UncheckedServletException;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.InputStreamReader;
+import java.nio.file.Files;
+import java.util.Objects;
 import org.apache.coyote.Processor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -26,10 +31,18 @@ public class Http11Processor implements Runnable, Processor {
 
     @Override
     public void process(final Socket connection) {
-        try (final var inputStream = connection.getInputStream();
-             final var outputStream = connection.getOutputStream()) {
+        try (final var inputStream = new InputStreamReader(connection.getInputStream());
+             final var outputStream = connection.getOutputStream();
+             final var bufferedReader = new BufferedReader(inputStream)) {
 
-            final var responseBody = "Hello world!";
+            // GET /index.html HTTP1.1
+            // 이렇게 들어오는 것을 분리
+            String[] inputs = bufferedReader.readLine()
+                    .split(" ");
+            String method = inputs[0];
+            String path = inputs[1];
+
+            final var responseBody = generateResponseBody(method, path);
 
             final var response = String.join("\r\n",
                     "HTTP/1.1 200 OK ",
@@ -44,4 +57,25 @@ public class Http11Processor implements Runnable, Processor {
             log.error(e.getMessage(), e);
         }
     }
+
+    private String generateResponseBody(final String method, final String path) throws IOException {
+        // base URL 이라면 문자열 반환
+        if ("GET".equals(method) && "/".equals(path)) {
+            return "Hello world!";
+        }
+
+        // index.html 반환
+        if ("GET".equals(method) && "/index.html".equals(path)) {
+            final File file = new File(Objects.requireNonNull(getClass()
+                            .getClassLoader()
+                            .getResource("static" + path))
+                    .getFile());
+
+            return new String(Files.readAllBytes(file.toPath()));
+        }
+
+        return "";
+    }
+
+
 }
